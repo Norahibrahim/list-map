275ed478c65fe8bba3a25d49c6fed364dbc083af
Norahibrahim <93065441+Norahibrahim@users.noreply.github.com>
2022-06-15 17:08:13 +0300

    Add files via upload

---
+++ dart_applicationhw1.dart
@@ -0,0 +1,15 @@
+
+String carName = "oudi";
+String? driverName = "sara" ;
+int idCar = 1020;
+Map Cars ={"carName":"mazda","driverName":"norah"};
+List  driverNames =[{"carName":"oudi","driverName":"sara","idCars":1020},
+{"carName":"mazda","driverName":"norah","idCars":1085},
+{"carName":"luxues","driverName":"waad","idCars":null}
+];
+
+main(){
+    print(driverNames[2]);
+}
+
+    
